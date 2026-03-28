d9b1b7359e58129699ca035393b0e79404ec697f
jmlo <jmlo.juanmasp@gmail.com>
2026-03-28 16:49:56 +0100

    Ficheros iniciales.

---
+++ Models/InventoryItem.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Logitrack.Models
+{
+  public class InventoryItem
+  {
+    [Key]
+    public int ItemId { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public int Quantity { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Location { get; set; } = string.Empty;
+
+    public int? OrderId { get; set; }
+
+    [ForeignKey(nameof(OrderId))]
+    public Order? Order { get; set; }
+
+    public string DisplayInfo()
+    {
+      return string.Format("Item: {0} | Quantity: {1} | Location: {2}", Name, Quantity, Location);
+    }
+  }
+}
